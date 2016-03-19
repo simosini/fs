5ad0b827c743c9f1359fda025a322810178b1b72
giosumarin <giosumarin@yahoo.com>
2016-03-19 18:54:07 +0100

    Added files via upload

---
+++ Scrivania/fs/ric_lista.fsx
@@ -0,0 +1,47 @@
+﻿let rec rmEven l =
+    match l with
+        | [] -> []
+        | h::tl when (h%2)=0 -> [h] @ rmEven tl
+        | h::tl -> rmEven tl
+
+let rec rmOddPos l =
+    match l with
+        | [] -> []
+        | h1::h2::tl -> [h1] @ rmOddPos tl
+        | h::tl -> [h]  @ rmOddPos tl
+
+let rec split l =
+    match l with
+        | [] -> ([],[])
+        | h::[] -> ([h],[])
+        | h1::h2::tl -> 
+            let (l1,l2)=split tl 
+            (h1::l1, h2::l2)
+
+let rec cmpLength(l1,l2) =
+    match (l1,l2) with
+        | ([],[]) -> 0
+        | (ls,[]) -> 1
+        | ([],ls) -> -1
+        | (h1::tl1,h2::tl2) -> cmpLength(tl1,tl2)
+
+
+// ???
+let rec remove(x, l) = 
+    match l with
+        | [] -> []
+        | h::tl when h=x -> h@remove(x,tl)
+        | h::tl ->  remove(x,tl)
+
+let rec downto0 x =
+    match x with
+        | 0 -> [0]
+        | _ -> [x]@downto0(x-1)
+
+let rec upto x =
+    match x with
+        | 0 -> [0]
+        | _ -> upto(x-1)@[x]
+
+
+
---
+++ Scrivania/fs/tag.fsx
@@ -0,0 +1,31 @@
+﻿type figura = 
+   | Rettangolo of float * float
+   | Quadrato of float   
+   | Triangolo of float * float;;
+
+let area fig =
+   match fig with
+   | Rettangolo(b,h) ->  
+        match (b,h) with
+            | (b, h) when b>0. && h>0. -> Some ( b * h )
+            | (_,_) -> None
+    | Quadrato lato ->
+        match lato with
+            | l when l>0. -> Some ( l * l )
+            | _ -> None
+    | Triangolo(b,h)  ->  
+        match (b,h) with
+            | (b, h) when b>0. && h>0. -> Some (( b * h ) / 2.)
+            | (_,_) -> None;;
+  
+let printArea fig=  
+    match area fig with
+        | Some k -> "area = "+(string k)
+        | None -> "errore" ;;
+
+
+let sommaArea (fig1, fig2)=
+    match (area fig1, area fig2) with 
+        | (Some x, Some y) -> Some (x+y)
+        | (_,_) -> None ;;  
+          
