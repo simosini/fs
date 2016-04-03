bdf76515ac2dd4d7b282de4191246133b5e56d67
simosini <simosini80@gmail.com>
2016-04-03 08:07:01 +0200

    add lab08 with homework test and exercises on records

---
+++ lab08/dc.fsx
@@ -0,0 +1,67 @@
+﻿#r "FsCheck";;
+open FsCheck;;
+
+type category =
+    | Daycare
+    | Nursery
+    | Recreation;;
+ 
+type name =  string;;
+type childDes = Desc of name * category;;
+
+let number (cat, l : childDes list) =
+     let rec aux n l =
+        match l with
+        | []                        -> n
+        | Desc(_,c)::t when c = cat -> aux (n+1) t
+        | _::t                      -> aux n t
+     aux 0 l;;
+
+let partTestNumber (l : childDes list, cat : category) =
+    number(cat, l) <= List.length l;;
+do Check.Quick partTestNumber;;
+
+let cost = function
+    | Daycare    -> 225.
+    | Nursery    -> 116.
+    | Recreation -> 110.;;
+//calculate price for extra children after first one
+let rec payhalf (n : name, l : childDes list) =
+    match l with
+    | []                               -> 0.
+    | Desc(name, cat)::t when name = n -> cost cat / 2. + payhalf (n, t)
+    | _::t                             -> payhalf (n, t);;
+       
+let rec pay (n : name, l : childDes list) =
+    match l with 
+    | []                               -> 0.
+    | Desc(name, cat)::t when name = n -> cost cat + payhalf (n, t)
+    | _::t                             -> pay (n, t);; 
+
+//create a list of all names of a childDes list
+let takeList (lst  : childDes list) = 
+    let rec helper mySet = function
+        | []                 -> Set.toList mySet
+        | Desc(name, cat)::t -> helper (Set.add name mySet) t
+    helper Set.empty lst;;
+
+// calculate total amount spent given a childDes list
+let total (l : childDes list) =
+    List.sum (List.map (fun x -> pay (x ,l)) (takeList l));;
+
+//remove name from list
+let rec removeName (n : name, l : childDes list) = 
+    match l with
+        | []                             -> []
+        | Desc(name, _)::t when name = n -> removeName (n, t)
+        | (Desc(name, cat) as m)::t      -> m::removeName (n, t);; 
+
+//same as total but with a different implementation in order to test function pay
+let rec totIncome = function
+    | []                      -> 0.
+    | Desc(name, cat)::t as l -> pay(name, l) + totIncome (removeName (name, l));; 
+
+let payTest (ls : childDes list) =
+    total ls = totIncome ls;;     
+ 
+do Check.Quick payTest;;
---
+++ lab08/esami.fsx
@@ -0,0 +1,41 @@
+﻿#r "FsCheck";;
+open FsCheck;;
+
+
+type valV = {studente : string ; voto: int};; 
+type valG = {nome : string     ; giudizio : string};;
+
+let valuta vV =
+    match vV.voto with
+    | n when n < 18 -> {nome = vV.studente ; giudizio = "insufficiente"}
+    | n when n < 23 -> {nome = vV.studente ; giudizio = "sufficiente"}
+    | n when n < 27 -> {nome = vV.studente ; giudizio = "buono"}
+    | _             -> {nome = vV.studente ; giudizio = "ottimo"};; 
+
+let rec valutaList ( l : valV list) =
+    match l with
+    | []   -> []
+    | h::t -> (valuta h)::valutaList t;;
+
+let ``valutaList è una map di valuta`` (xs : valV list)  =
+  List.map valuta xs = valutaList xs;;
+do Check.Quick  ``valutaList è una map di valuta``;;
+
+let rec creaValList (l1 : string list, l2 : int list) =
+    match l1, l2 with
+    | ([],[]) | ([],_) | (_,[]) -> []
+    | (h1::t1, h2::t2)          -> {studente = h1 ; voto = h2}:: creaValList (t1, t2);;
+    
+let media (l : valV list) = 
+    let rec sommaAndConta (tot, count) = function
+        | []   -> (float tot) / (float count)
+        | h::t -> sommaAndConta (tot + 1, count + h.voto) t
+    if List.isEmpty l then 0.0 else sommaAndConta (0,0) l;;
+
+let separa (l : valV list) = List.partition(fun x -> x.voto < 18) l;;
+
+let ``due liste risultato hanno stessi elementi di vs`` (l : valV list) =
+    let l1,l2 = separa l   
+    List.sort l = List.sort (l1@l2);;
+
+do Check.Quick ``due liste risultato hanno stessi elementi di vs``;;
