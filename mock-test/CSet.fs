a98dbf4f805151eae4a61cac7bf99a955c781499
simosini <simosini80@gmail.com>
2016-05-29 20:31:41 +0200

    Small changes in CSet file

--- mock-test/CSet.fs
+++ mock-test/CSet.fs
@@ -13,3 +13,10 @@ let add el set = S(fun x -> contains x (singleton el) || contains x set)
 let union set1 set2 = S(fun x -> contains x set1 || contains x set2)
 
 let ofList l = List.fold (fun set el -> add el set) empty l     
+
+let toList s max =  
+    [ for x in 0 .. max do if (contains x s) then yield x ]
+
+let b = empty |> add 5 |> add 6 |> add 2
+let lst = toList b 7 // int list = [2; 5; 6]
+
